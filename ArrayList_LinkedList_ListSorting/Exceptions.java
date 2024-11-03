56ccf5e698fe82cc2a2ef46a5d50c49f07ae1261
Phenny <mwaisakaphenny@gmail.com>
2024-11-03 03:17:44 +0300

    some practice sapmles and notes

---
+++ ArrayList_LinkedList_ListSorting/Exceptions.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Exceptions {
+    static void checkAge(int age){
+        if (age < 18){
+            throw new ArithmeticException("Access denied - you must be at least 18 years old");
+        }
+        else{
+            System.out.println("Access granted - you are old enough!");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age to proceed: ");
+        int age = scanner.nextInt();
+
+        checkAge(age);
+    }
+}
+
+/*
+Error/Exception	Description
+ArithmeticError	Occurs when a numeric calculation goes wrong
+ArrayIndexOutOfBoundsException	Occurs when trying to access an index number that does not exist in an array
+ClassFormatError	Occurs when a class file cannot be accessed
+ClassNotFoundException	Occurs when trying to access a class that does not exist
+ConcurrentModificationException	Occurs when an element is added or removed from iterables
+FileNotFoundException	Occurs when a file cannot be accessed
+IncompatibleClassChangeError	Occurs when there's been a change in a base class after a child class has already been initialized
+InputMismatchException	Occurs when entering wrong input (e.g. text in a numerical input)
+InterruptedException	Occurs when a Thread is interrupted while waiting/sleeping
+InvalidClassException	Occurs when the Serialization runtime observes a problem with a class
+IOException	Occurs when an input or output operation fails
+NegativeArraySizeException	Occurs when trying to create an array with negative size
+NoClassDefFoundError	Occurs when the class is not found at runtime
+NoSuchFieldException	Occurs when trying to access a class field/variable that does not exist
+NoSuchMethodException	Occurs when trying to access a class method that does not exist
+NullPointerException	Occurs when trying to access an object referece that is null
+NumberFormatException	Occurs when it is not possible to convert a specified string to a numeric type
+RuntimeException	Occurs when an exception occurs at runtime
+StringIndexOutOfBoundsException	Occurs when trying to access a character in a String that does not exist
+TypeNotPresentException	Occurs when a type cannot be found
+IllegalArgumentException	Occurs when when an illegal argument is passed to a method
+IllegalStateException	Occurs when when a method is called at an illegal time
+ */
--- ArrayList_LinkedList_ListSorting/RegexSample.java
+++ ArrayList_LinkedList_ListSorting/RegexSample.java
@@ -1,6 +1,21 @@
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+public class RegexSample{
+    public static void main(String[] args) {
+        String regex = "\\d+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher("The price is 200 dollars");
+
+        while(matcher.find()){
+            System.out.println("Found number: "+ matcher.group());
+        }
+
+        String result = matcher.replaceAll("[number]");
+        System.out.println("Modified String: "+ result);
+    }
+}
+/* 
 public class RegexSample {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("w3schools", Pattern.CASE_INSENSITIVE);
@@ -16,11 +31,71 @@ public class RegexSample {
     }
 }
 
+*/
+
+
 /*
 contained in the java.util.regex package, contains 3 classes;
 Pattern class; compiles a regular expression pattern that can be used in text searches
 Matcher Class; used to search or match the compiled Pattern within a specific string
 PatternSyntaxException Class; indicates syntax errors in the pattern.
+
+Basic Syntax for Java Regex Program
+
+//import the classes
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class RegexExample {
+    public static void main(String[] args) {
+        //compile the regex pattern using Pattern.compile()
+        Pattern pattern = Pattern.compile("pattern here");
+        //create Matcher object by passing the input string to Pattern.matcher()
+        Matcher matcher = pattern.matcher("input string here");
+
+        //Use Matcher methods like .find(), .matches(), or .replaceAll() to interact with the pattern.
+
+        while (matcher.find()) {
+            System.out.println("Match found: " + matcher.group());
+        }
+    }
+}
+
+Common Methods in Matcherand Pattern Classes
+boolean find() : searches for the next subsequence that matches the pattern.
+boolean matches(): checks if ther entire input sequence matches the pattern.
+String replaceAll(String replacement): Replaces all occurrences of the pattern with the provided string.
+String group(): returns the matched subsequence.
+
+Flags for Pattern Compilation
+-flags modify how the regex search is performed
+Pattern.CASE_INSENSITIVE: Ignores case
+Pattern.LITERAL; Treats special characters as normal characters.
+Pattern.UNICODE_CASE; works with CASE_INSENSITIVE to ignore case across Non-English alphabets.
+
+Core Regular Expression Patterns
+Brackets - used to find specific character ranges:
+[abc] -matches any one of the characters
+[^abc] -matches any character except a,b, and c
+[0-9] -matches any digit from 0-9
+
+MetaCharacters - have special meanings
+| :matches any one of the patterns e.g. cats|dogs
+. :match any character except newline.
+^ :matches the start of a string
+$ :matches the end of a string
+\d: matches any digit (0-9)
+\s: matches any whitespace character
+\b: matches at the beginning or end of a word i.e \bword, word\b 
+
+Quantifiers
+n+: matches one or more occurrences of n.
+n*: matches zero or more occurrences of n.
+n?: matches zero or one occurrence of n.
+n{x}: matches exactly x occurrences of n.
+n{x,y}: matches between x and y occurrences.
+n{x,} : matches at least x occurrences.
+
  * Applications of Java Regular Expressions
  * Input validation; validating formats like emails, phonenumbers, postal codes.
  * Data Extraction; Extracting specific data like dates, urls, or numbers from text
